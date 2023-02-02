2545d415e45e48bd5b53aa64705094bcef88f208
honghui <wanghonghui_work@163.com>
2023-02-02 16:08:35 +0800

    addOn:CommandLineRunner预加载

---
+++ src/main/java/com/example/demo/init/InitRunner2.java
@@ -0,0 +1,21 @@
+package com.example.demo.init;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author honghui 2023/02/02
+ */
+@Slf4j
+@Component
+@Order(value = 2)
+public class InitRunner2 implements CommandLineRunner {
+
+  @Override
+  public void run(String... args) {
+    log.info(">>>>>>>>>>>>>>>服务启动执行，执行加载数据等操作 InitRunner2 order 2 <<<<<<<<<<<<<");
+  }
+
+}
